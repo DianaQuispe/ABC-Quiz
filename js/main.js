910da4e01e5b25174a7d1557f5b54e5bf3b00520
MyDiana <danaalienmas@gmail.com>
2017-09-16 11:27:14 -0500

    cambios de texto

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 function myFunction(x) {
   x.classList.toggle("fa-thumbs-down");
 }
+
+let plane = ["images/plane.svg","Which is the oldest airline in the world?",['Avianca','KLM', 'Qantas']];
+let male = ["images/ship.svg","Which is the largest port in the world?",['A Port of Shanghai',' Port of Singapore', 'Port of Rotterdam']];
+let s = ["images/bycicle.svg","What is the longest distance cycling backwards?",['89.30km','675.10km', '337.60km']];
+for( var i =0; i < plane.length; i++) {
+  $('#imagenes').append('<h1>'+ plane[1]+ '</h1>')
+  $('#questions').append('<h1>'+ plane[1]+ '</h1>')
+}
+
+
